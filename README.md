ed8330527d1e6d8324e5bb21f19118b0df5ce4d9
DF_XYZ <dfxyz1@gmail.com>
2019-04-24 10:41:06 +0800

    add cmd script to add or remove context menu items

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # MySYS64
 
 ## Setup
+Basic steps:
 1. Extract 'msys64' here (the same directory of 'README.md');
 2. Run 'terminal.exe' and execute 'setup.sh';
 3. Restart 'terminal.exe';
 4. Done!
 
-If any basic package like 'filesystem' or 'pacman' is upgraded,
+If any core package like 'filesystem' or 'pacman' is upgraded,
 'setup.sh' is required to execute again.
+
+Optional:
+* Execute 'context_menu_items.cmd' as administartors to add or remove 
+MySYS64's context menu items
---
+++ context_menu_items.cmd
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+chcp 65001 >nul 2>&1
+
+set itemName="在此处打开 MySYS64 终端(&T)"
+set exePath=%~dp0%terminal.exe
+set regPath1=HKCR\Directory\shell\MySYS64
+set regPath2=HKCR\Directory\background\shell\MySYS64
+
+reg query %regPath1% /s >nul 2>&1
+if %errorlevel% == 0 (
+    reg delete %regPath1% /f >nul 2>&1
+    reg delete %regPath2% /f >nul 2>&1
+) else (
+    reg add %regPath1% /ve /d %itemName% >nul 2>&1
+    reg add %regPath1%\command /ve /d %exePath% >nul 2>&1
+    reg add %regPath2% /ve /d %itemName% >nul 2>&1
+    reg add %regPath2%\command /ve /d %exePath% >nul 2>&1
+)
